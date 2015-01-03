cdfee4cc7f246c1aa2f35e2184aae29cd68dc535
Sam Meech-Ward <sam@meech-ward.com>
2015-01-03 17:57:03 +0000

    Initial Commit

---
+++ SMWAccordion/SMWAccordion.h
@@ -0,0 +1,13 @@
+//
+//  SMWAccordion.h
+//  SMWAccordion
+//
+//  Created by Sam Meech Ward on 2015-01-03.
+//  Copyright (c) 2015 Sam Meech-Ward. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMWAccordion : NSObject
+
+@end
---
+++ SMWAccordion/SMWAccordion.m
@@ -0,0 +1,13 @@
+//
+//  SMWAccordion.m
+//  SMWAccordion
+//
+//  Created by Sam Meech Ward on 2015-01-03.
+//  Copyright (c) 2015 Sam Meech-Ward. All rights reserved.
+//
+
+#import "SMWAccordion.h"
+
+@implementation SMWAccordion
+
+@end
